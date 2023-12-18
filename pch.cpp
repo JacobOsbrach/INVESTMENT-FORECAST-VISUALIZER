9b9447025a13a7969c2a684d0b63385b62cd7ea0
JacobOsbrach <154211082+JacobOsbrach@users.noreply.github.com>
2023-12-17 22:40:52 -1000

    Add files via upload

---
+++ pch.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.h"
+
+#include <iostream>
+using namespace std;
+
+
+Item::Item(double ainitial, double adeposit, double ainterest, double ayears)
+{
+    setInitial(ainitial);
+    setDeposit(adeposit);
+    setInterest(ainterest);
+    setYears(ayears);
+}
+void Item::setInitial(double ainitial) {
+    
+    initial = ainitial;
+}
+void Item::setDeposit(double adeposit) {
+    deposit = adeposit;
+}
+void Item::setInterest(double ainterest) {
+    interest = ainterest;
+}
+void Item::setYears(double ayears) {
+    years = ayears;
+}
+double Item::getDeposit() {
+
+    if (typeid(deposit).name() == "c")
+    {
+        deposit = NULL;
+    }
+    else
+
+    return deposit;
+}
+double Item::getInterest() {
+
+    if (typeid(interest).name() == "c")
+    {
+        interest = NULL;
+    }
+    else
+    return interest;
+}
+double Item::getInitial() {
+
+    if (typeid(initial).name() == "c")
+    {
+        initial = NULL;
+    }
+    else
+        return initial;
+    
+}
+double Item::getYears() {
+
+    if (typeid(years).name() == "c")
+    {
+        years = NULL;
+    }
+    else
+    return years;
+}
---
+++ pch.h
@@ -0,0 +1,25 @@
+﻿#ifndef ITEM_TO_calculate_H
+#define ITEM_TO_calculate_H
+
+#include <string>
+using namespace std;
+
+class Item {
+private:
+	double initial;
+	double deposit;
+	double interest;
+	double years;
+public:
+	Item (double ainitial, double adeposit, double acost, double ayears);
+	void setInitial(double ainitial);
+	void setDeposit(double adeposit);
+	void setInterest(double ainterest);
+	void setYears(double ayears);
+	double getInitial();
+	double getDeposit();
+	double getInterest();
+	double getYears();
+
+};
+#endif
